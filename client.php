ec038088891ae58dadf28069125537504f8dcc5d
ojoseph <olivier07joseph@gmail.com>
2014-11-27 10:12:56 +0900

    Trying something new
    Ask for an engineer for some guidance.

--- client.php
+++ client.php
@@ -2,10 +2,12 @@
   include("lib/nusoap.php");
   $client = new soapclient("http://localhost:8888/WebServiceSOAP_2/server.php?wsdl");
   $result    =    $client->gethelloworld("HELOOO");
-  //$result_test    =    $client->gettestifworks("NUMBER 2");
+  $result_test    =    $client->gettestifworks("NUMBER 2");
 
   echo "<pre>";
   print_r($result);
+  echo "<br/><br/><br/>";
+  print_r($result_test);
   echo "</pre>";
 
 ?>
---
+++ getPost.php
@@ -0,0 +1,20 @@
+<?php
+
+$notification = $_POST['notification'];
+//$package = $_POST['package'];
+//$notification = $_POST['notification'];
+//$notification = $_POST['notification'];
+
+if($notification) {
+  echo("POST VALUE:  ".$notification." ");
+}
+
+
+
+$unitypurchase = $_POST['$unitypurchase'];
+
+if($unitypurchase) {
+  echo("POST VALUE:  ".$unitypurchase." ");
+}
+
+?>
